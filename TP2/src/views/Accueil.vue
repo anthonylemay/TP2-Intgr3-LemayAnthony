2361c5f58407c1ae4c01b9d0644152b6b9f13535
anthonylemay <59809394+anthonylemay@users.noreply.github.com>
2023-12-29 17:11:53 -0500

    tests inject

--- TP2/src/views/Accueil.vue
+++ TP2/src/views/Accueil.vue
@@ -1,45 +1,30 @@
 <template>
-    <h1>Accueil</h1>
-    <img src="https://images.unsplash.com/photo-1542751371-adc38448a05e?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="image d'un gamer à l'ordinateur">
-    <h2>Ligue eSports</h2>
-    <h3>Le jeu à un autre niveau</h3>
-    <div v-for="game in games" :key="game.idGame">
-    <img :src="game.gamePicture" :alt="'Image du jeu ' + game.gameName">
+  <h1>Accueil</h1>
+  <img
+    src="https://images.unsplash.com/photo-1542751371-adc38448a05e?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    alt="image d'un gamer à l'ordinateur"
+  />
+  <h2>Ligue eSports</h2>
+  <h3>Le jeu à un autre niveau</h3>
+  <div v-for="game in games" :key="game.idGame">
+    <img :src="game.gamePicture" :alt="'Image du jeu ' + game.gameName" />
     <h2>{{ game.gameName }}</h2>
-    <p>{{ game.description }}
-    </p>
-</div>
-
+    <p>{{ game.description }}</p>
+  </div>
 </template>
 
 <script>
 export default {
-    props:{
-    ajoutEquipe: Function,
-    equipes: Array,
-    games:{
+  inject: {
+    games: {
       type: Array,
       required: true,
       default: () => [
-        {gameName: String,
-        required: true,
-        default: ""
-        },
-        {gamePicture: String,
-        required: true,
-        default: ""
-        },
-        {description: String,
-        required: true,
-        default: ""
-        }
+        { gameName: String, required: true, default: '' },
+        { gamePicture: String, required: true, default: '' },
+        { description: String, required: true, default: '' }
       ]
     }
-}
-,
-
-  mounted() {
-    console.log(this.games); // Debug
   }
 }
 </script>
--- TP2/src/views/DetailsEquipe.vue
+++ TP2/src/views/DetailsEquipe.vue
@@ -1,30 +1,28 @@
 <template>
-    <div v-if="detailsEquipe">
-      <h1>Détails de l'équipe {{ detailsEquipe.nom }}</h1>
-      <img :src="detailsEquipe.logo" :alt="'Logo de l\'équipe ' + detailsEquipe.nom">
-      <h2>{{ detailsEquipe.nom }}</h2>
-      <ul>
-        <li v-for="joueur in detailsEquipe.joueurs" :key="joueur.nomJoueur">{{ joueur.nomJoueur }}</li>
-      </ul>
-    </div>
+  <div v-if="detailsEquipe">
+    <h1>Détails de l'équipe {{ detailsEquipe.nom }}</h1>
+    <img :src="detailsEquipe.logo" :alt="'Logo de l\'équipe ' + detailsEquipe.nom" />
+    <h2>{{ detailsEquipe.nom }}</h2>
+    <ul>
+      <li v-for="joueur in detailsEquipe.joueurs" :key="joueur.nomJoueur">
+        {{ joueur.nomJoueur }}
+      </li>
+    </ul>
+  </div>
+</template>
 
-  </template>
-  
-  <script>
-
-  export default {
-
-    props: ['equipes'],
-    data() {
-      return {
-        idEquipe: this.$route.params.id
-      }
-    },
-    computed: {
-      detailsEquipe() {
-        return this.equipes.find(equipe => equipe.idEquipe == this.idEquipe);
-      }
+<script>
+export default {
+  inject: ['equipes'],
+  data() {
+    return {
+      idEquipe: this.$route.params.id
+    }
+  },
+  computed: {
+    detailsEquipe() {
+      return this.equipes.find((equipe) => equipe.idEquipe == this.idEquipe)
     }
   }
-  </script>
-  
+}
+</script>
